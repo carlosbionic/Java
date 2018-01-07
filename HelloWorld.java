999feb424cd13083d28b8d6600a28948ca917abd
carlosbionic <carlosaprado@gmail.com>
2018-01-07 19:39:05 -0300

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld {
+
+ /* Primer programita de Java.
+  * Esto va a imprimir 'Hola Ke Ase' en la salida
+  */
+
+ public static void main(String []args) {
+  System.out.println("Hola Ke Ase"); //imprime Hola Ke Ase
+ }
+}
